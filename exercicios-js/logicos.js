9086856810a9cf04bdaa8203ffa2fb2a6baab40c
4grel4 <gabriel_agrela2020@hotmail.com>
2023-05-02 15:22:52 -0300

    Finalmente, os ultimos Fundamentos

---
+++ exercicios-js/logicos.js
@@ -0,0 +1,13 @@
+function compras(trabalho1, trabalho2) {
+    const comprarSorvete = trabalho1 || trabalho2
+    const comprarTv50 = trabalho1 && trabalho2 //  se o primeiro for falso = falso
+    //const comprarTv32 = !!(trabalho1 ^ trabalho2) // bitwise xor
+    const comprarTv32 = trabalho1 != trabalho2
+    const manterSaudavel = !comprarSorvete // operador unário
+    return { comprarSorvete, comprarTv50, comprarTv32, manterSaudavel} // recurso novo ECMA2015
+}
+
+console.log(compras(true, true))
+console.log(compras(true, false))
+console.log(compras(false, true))
+console.log(compras(false, false))
