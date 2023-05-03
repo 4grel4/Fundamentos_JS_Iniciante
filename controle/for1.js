41052dd34dc4ac6c8e4c90cffcb7d019903393c3
4grel4 <gabriel_agrela2020@hotmail.com>
2023-05-03 10:59:04 -0300

    Aprendendo tipos de funções (Básico)

---
+++ controle/for1.js
@@ -0,0 +1,14 @@
+let contador = 1 //  contador
+while(contador <= 10) { // expressão
+    console.log(`contador = ${contador}`)
+    contador++ // incremento
+}
+
+for(let i = 1; i <= 10; i++) { 
+    console.log(`i = ${i}`)
+}
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7] // percorrendo uma array.
+for (let i = 0; i < notas.length; i++) {
+    console.log(`nota = ${notas[i]}`)
+}
