a26db8d268d4731484b7ca025599fbf57e5e9ada
4grel4 <gabriel_agrela2020@hotmail.com>
2025-04-22 14:53:47 -0300

    Retornando a este curso, foco em terminar -- Mais alguns exemplos.

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Ualeskah', 'Cibalena']
+const filhos = ['Gabriel', 'Arnaldo']
+const todos = filhas.concat(filhos)
+console.log(todos, filhos, filhas)
+
+console.log(['agrela'].concat([1, 2], [3, 4], 5, [[6, 7]]))
